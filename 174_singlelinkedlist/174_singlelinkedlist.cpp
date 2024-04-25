5352dee2ca2462a16044c11d87cd52c539ef3aa8
thekill022 <gibfathoni@gmail.com>
2024-04-25 12:54:41 +0700

    membuat prosedur addNode()

--- 174_singlelinkedlist/174_singlelinkedlist.cpp
+++ 174_singlelinkedlist/174_singlelinkedlist.cpp
@@ -11,6 +11,18 @@ struct Node {
 //set value START = NULLL
 Node* START = NULL;
 
+//membuat prosedur addNode()
+void addNode() {
+    int nim;
+    string nama;
+    Node* nodeBaru = new Node();
+    cout << "Masukkan NIM: ";
+    cin >> nim;
+    cout << "Masukkan Nama: ";
+    cin >> nama;
+    nodeBaru->noMhs = nim;
+    nodeBaru->name = nama;
+};
 
 int main()
 {
